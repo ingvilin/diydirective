efd247e30484e033e50b8ad257badcac8dc62793
ingvild <ingvildindr@gmail.com>
2014-09-08 22:15:55 +0200

    styling og watch endringer

--- watch.js
+++ watch.js
@@ -2,16 +2,17 @@
     
     app.directive('twitterDirective', function(){
       return{ 
-         template: '<p> {{teller}} </p>',
+        scope: true,
+         template: '<p> {{teller}} {{tweet}} </p>',
         link: function (scope) {
             scope.teller = 140;
 
             scope.$watch(function () {
                 return scope.tweet;
-            }, function (newValue, oldValue) {
+            }, function (newValue) {
                 scope.teller = 140 - newValue.length;
-                return newValue;
             });
         }
       };
 }); 
+
