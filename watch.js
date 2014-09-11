ccd7c192de5b31322afd45e07c033cbacb57434f
ingvild <ingvildindr@gmail.com>
2014-09-11 22:25:58 +0200

    Oppdatert fasit - watch

--- watch.js
+++ watch.js
@@ -1,18 +1,21 @@
   var app = angular.module('directiveDemo');
-    
-    app.directive('twitterDirective', function(){
-      return{ 
-        scope: true,
-         template: '<p> {{teller}} </p>',
-        link: function (scope) {
-            scope.teller = 140;
 
-            scope.$watch(function () {
-                return scope.tweet;
-            }, function (newValue) {
-                scope.teller = 140 - newValue.length;
-            });
-        }
-      };
-}); 
+  app.directive('twitterDirective', function(){
+    return{ 
+      scope: true,
+      template: '<p> {{teller}} </p>',
+      link: function (scope) {
+        scope.teller = 140;
+
+        scope.$watch(function () {
+          return scope.tweet;
+        }, function (newValue) {
+          scope.teller = 140;
+          if(newValue){
+            scope.teller = 140 - newValue.length;
+          }
+        });
+      }
+    };
+  }); 
 
