e131d4f19e42ac771528eb7e3657fe9ae82bd1fe
ingvild <ingvildindr@gmail.com>
2014-09-11 22:36:05 +0200

    Watchoppgave klar

--- watch.js
+++ watch.js
@@ -2,18 +2,7 @@
     
     app.directive('twitterDirective', function(){
       return{ 
-        scope: true,
-         template: '<p> {{teller}} </p>',
-        link: function (scope) {
-            scope.teller = 140;
-            console.log(scope.tweet)
 
-            scope.$watch(function () {
-                return scope.tweet;
-            }, function (newValue) {
-                scope.teller = 140 - newValue.length;
-            });
-        }
       };
 }); 
 
