d06253731b86ac707b1c191ab93c068ffa655d69
Tine Kleivane <kleivane@gmail.com>
2014-09-11 19:32:44 +0200

    Implementasjon demo-button

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
   var app = angular.module('directiveDemo', []);
     
     app.directive('demoButton', function(){
-      return { 
+      return {
+      	link: function(scope, element, attrs){
+      		scope.buttonTitle = attrs.title || "Send";
+      	},
+      	scope: true,
+		template: '<button type="button" class="btn btn-default btn-default"><span class="glyphicon glyphicon-send"></span> {{buttonTitle}}</button>' 
       };
 }); 
